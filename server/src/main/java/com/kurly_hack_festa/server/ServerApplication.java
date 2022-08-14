512f2ca7d165cbbb3bf7e785eaa95cd39823996b
Cha-Young-Ho <jkijki1249@gmail.com>
2022-08-15 00:16:23 +0900

    Docs : spring initialize

---
+++ server/src/main/java/com/kurly_hack_festa/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.kurly_hack_festa.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}

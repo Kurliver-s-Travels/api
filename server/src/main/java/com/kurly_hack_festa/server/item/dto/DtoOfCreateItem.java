1fc1bc591ae137a4fdaf6596a1c042da2bdb4970
Cha-Young-Ho <jkijki1249@gmail.com>
2022-08-16 15:27:39 +0900

    Feature : Item Entity의 배송날짜 추가

--- server/src/main/java/com/kurly_hack_festa/server/item/dto/DtoOfCreateItem.java
+++ server/src/main/java/com/kurly_hack_festa/server/item/dto/DtoOfCreateItem.java
@@ -4,6 +4,8 @@ import com.kurly_hack_festa.server.item.Location;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 public class DtoOfCreateItem {
@@ -11,4 +13,5 @@ public class DtoOfCreateItem {
     private String name;
     private int count;
     private Location location;
+    private LocalDateTime deliveryTime;
 }
